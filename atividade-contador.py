e54b793cdf77cd0c115da01dbd14b6c7511ad8e1
Vinicinhos11 <viniciusferreiran25@gmail.com>
2025-03-13 14:08:08 -0300

    Add files via upload

---
+++ atividade-contador.py
@@ -0,0 +1,7 @@
+import os
+os.system("cls")
+
+contador = 10
+while contador <= 200:
+    print("CONTADOR: ", contador)
+    contador +=1
---
+++ atividade-contadordecrescente.py
@@ -0,0 +1,8 @@
+import os 
+os.system("cls")
+
+contador = 200
+
+while contador >=10:
+    print("CONTADOR: ", contador)
+    contador -=1
